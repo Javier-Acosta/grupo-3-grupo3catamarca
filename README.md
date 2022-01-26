b2011708e8d6c80b8fe8cddc3f8c8a620a6495d6
Javier-Acosta <jacosta@catamarca.gov.ar>
2022-01-26 17:03:55 -0300

    Actualización

--- README.md
+++ README.md
@@ -3,8 +3,6 @@
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.1.4.
 
 ## Development server
-Para que funcione la app Valan, tienen que instalar npm.
-En la consola ejecuten el comando npm install
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {  RouterModule} from '@angular/router';
+
+
+// permite hacer peticiones http 
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SesionComponent } from './components/sesion/sesion.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+// importar rutas
+import { ROUTES } from './app.routes';
+import { RegistroComponent } from './components/registro/registro.component';
+import { SomosComponent } from './components/somos/somos.component';
+import { AyudaComponent } from './components/ayuda/ayuda.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SesionComponent,
+    NavbarComponent,
+    RegistroComponent,
+    SomosComponent,
+    AyudaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot( ROUTES, {useHash:true})
   ],
-  providers: [],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
