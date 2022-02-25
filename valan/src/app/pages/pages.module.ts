174a5af6097ef9cc777f765e432ba8f4aa866b10
Javier-Acosta <jacosta@catamarca.gov.ar>
2022-02-25 01:27:51 -0300

    Actualización portada

--- valan/src/app/pages/pages.module.ts
+++ valan/src/app/pages/pages.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ForoComponent } from './foro/foro.component';
 import { PortadaComponent } from './portada/portada.component';
+import { Portada2Component } from './portada2/portada2.component';
 
 
 
 @NgModule({
   declarations: [
     ForoComponent,
-    PortadaComponent
+    PortadaComponent,
+    Portada2Component
   ],
   exports:[
     ForoComponent,
-    PortadaComponent
+    PortadaComponent,
+    Portada2Component
   ],
   imports: [
     CommonModule
