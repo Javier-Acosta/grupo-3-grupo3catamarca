4a37c38c7a33a299770e5b17d6373e96c95fb46e
Javier-Acosta <jacosta@catamarca.gov.ar>
2022-02-25 20:59:52 -0300

    Portada V1.0

--- valan/src/app/pages/pages.module.ts
+++ valan/src/app/pages/pages.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { ForoComponent } from './foro/foro.component';
 import { PortadaComponent } from './portada/portada.component';
 import { Portada2Component } from './portada2/portada2.component';
+import { Portada3Component } from './portada3/portada3.component';
+import { RegistroComponent } from './registro/registro.component';
+import { RegistrosComponent } from './registros/registros.component';
 
 
 
@@ -10,12 +13,16 @@ import { Portada2Component } from './portada2/portada2.component';
   declarations: [
     ForoComponent,
     PortadaComponent,
-    Portada2Component
+    Portada2Component,
+    Portada3Component,
+    RegistroComponent,
+    RegistrosComponent
   ],
   exports:[
     ForoComponent,
     PortadaComponent,
-    Portada2Component
+    Portada2Component,
+    Portada3Component
   ],
   imports: [
     CommonModule
---
+++ valan/src/app/pages/registros/registros.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+//registro
+
+@Component({
+  selector: 'app-registros',
+  templateUrl: './registros.component.html',
+  styleUrls: ['./registros.component.css']
+})
+export class RegistrosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
